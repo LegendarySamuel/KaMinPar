c50588f7f741459d1adbc1f31eb8c5e1212468b5
SamuelGil <samuel-gil@gmx.de>
2023-06-06 13:53:40 +0200

    removed initialize; improved calculate_new_cluster

--- dkaminpar/coarsening/clustering/my_lp_clustering.cc
+++ dkaminpar/coarsening/clustering/my_lp_clustering.cc
@@ -23,41 +23,65 @@ namespace kaminpar::dist {
     }
 
     GlobalNodeID calculate_new_cluster(NodeID node, const DistributedGraph &graph, MyLPClustering::ClusterArray clusters) {
-        // find adjacent nodes
-        // NodeID degree = graph.degree(node);
+        /* find adjacent nodes
+         * calculate cluster with maximum intra cluster edge weight
+         * check weight of new edges and sum them up if in the same cluster, 
+         * then choose cluster with the highest gain in weight
+         * make sure max cluster weight constraint is not violated
+         * if best cluster is too heavy, choose next lighter one
+         */
         typedef std::pair<GlobalNodeID, EdgeWeight> clusterWeight;
         std::vector<clusterWeight> sums(0);
 
         for (auto&& edgeID : graph.incident_edges(node)) {
             NodeID target = graph.edge_target(edgeID);
             EdgeWeight eweight = graph.edge_weight(edgeID);            
+            GlobalNodeID clusterID = 0;
 
             if (graph.is_ghost_node(target)) {
-                graph.local_to_global_node(target);
                 graph.ghost_owner(target);
-                // TODO get clusterID from ghost owner
+                // TODO get clusterID from ghost owner (label)
+                // TODO set clusterID
+            } else {
+                clusterID = clusters[target];
             }
 
             std::unordered_map<GlobalNodeID, EdgeWeight> sums;
-            if (sums.find(clusters[target]) != sums.end()) {    // cluster is represented in sums
-                EdgeWeight temp = sums[clusters[target]] + eweight;
-                sums.erase(clusters[target]);
-                sums.insert({clusters[target], temp});     // different Syntax than that of the else block, just cause
-            } else {    // cluster is not represented in sums
-                sums.insert(std::make_pair(clusters[target], eweight));
+            if (sums.find(clusterID) != sums.end()) {    // cluster is already represented in sums
+                EdgeWeight temp = sums[clusterID] + eweight;
+                sums.erase(clusterID);
+                sums.insert(std::make_pair(clusterID, temp));
+            } else {    // cluster is not yet represented in sums
+                sums.insert(std::make_pair(clusterID, eweight));
             }
         }
-        // calculate cluster with maximum intra cluster edge weight
-        // check weight of new edges and sum them up if in the same cluster, then choose cluster with the highest gain in weight
 
-        // make sure max cluster weight constraint is not violated
-        // if best cluster is too heavy, choose next lighter one
+        std::vector<clusterWeight> order(0);
+        for (auto&& pair : sums)  {
+            order.push_back(pair);
+        }
+
+        // TODO order vector by weight
+        // TODO check for constraint
+        // TODO return new clusterID
+
     }
 
     ///////////////////////////////////////////////////////////////////////////////
 
     void MyLPClustering::initialize(const DistributedGraph &graph) {
 
+    }
+
+    // subgraphs are already given by initial partitioning
+    /* have to calculate clusters:
+     * 1.) initialize clusters with single nodes
+     * 2.) for each node maximize intra cluster edge weight by moving node to adjacent cluster without exceeding max cluster weight
+     * (have to check for all nodes at the edges of clusters, not only for interface nodes)
+     * 3.) put all isolated nodes in one cluster
+     */
+    MyLPClustering::ClusterArray &MyLPClustering::cluster(const DistributedGraph &graph, GlobalNodeWeight max_cluster_weight) {
+        // TODO
         MyLPClustering::ClusterArray clusters(graph.n());
         typedef MyLPClustering::ClusterArray::iterator iter;
         
@@ -75,17 +99,6 @@ namespace kaminpar::dist {
         int end = rank*nOP + (graph.n()/nOP);
         */
 
-        // communicate labels
-    }
-
-    // subgraphs are already given by initial partitioning
-    /* have to calculate clusters:
-     * 1.) initialize clusters with single nodes
-     * 2.) for each node maximize intra cluster edge weight by moving node to adjacent cluster without exceeding max cluster weight
-     * (have to check for all nodes at the edges of clusters, not only for interface nodes)
-     * 3.) put all isolated nodes in one cluster
-     */
-    MyLPClustering::ClusterArray &MyLPClustering::cluster(const DistributedGraph &graph, GlobalNodeWeight max_cluster_weight) {
-        // TODO
+        // TODO communicate labels
     }
 }
